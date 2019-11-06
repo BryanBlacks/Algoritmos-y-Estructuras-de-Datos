ef3ad6e5202a043e24a2901ba40effb8381a718d
benedetto597 <ejbg597@gmail.com>
2019-11-05 22:02:23 -0600

    Pantallas actualizadas, 3 de 6 hechas

---
+++ Unidad I/PAC III 2019/Proyecto 1/Python/Interface/EditWindow.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'TableWin.ui'
+#
+# Created by: PyQt5 UI code generator 5.5.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Tabla(object):
+    def setupUi(self, Tabla):
+        Tabla.setObjectName("Tabla")
+        Tabla.resize(595, 388)
+        Tabla.setStyleSheet("background-color: rgb(211, 205, 255);")
+        self.txtTable = QtWidgets.QTextEdit(Tabla)
+        self.txtTable.setGeometry(QtCore.QRect(20, 20, 551, 261))
+        font = QtGui.QFont()
+        font.setFamily("Bitstream Vera Sans")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.txtTable.setFont(font)
+        self.txtTable.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.txtTable.setObjectName("txtTable")
+        self.btnEdit = QtWidgets.QPushButton(Tabla)
+        self.btnEdit.setGeometry(QtCore.QRect(230, 300, 140, 60))
+        font = QtGui.QFont()
+        font.setFamily("Bitstream Vera Sans")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.btnEdit.setFont(font)
+        self.btnEdit.setStyleSheet("background-color: qlineargradient(spread:reflect, x1:0, y1:0.882, x2:0.851, y2:0, stop:0 rgba(204, 0, 0, 0), stop:0.273632 rgba(255, 255, 255, 255));\n"
+"background-color: rgb(147, 125, 255);\n"
+"color: rgb(0, 0, 0);\n"
+"border-style: solid;\n"
+"border-width:2px;\n"
+"border-radius:20px;\n"
+"border-color: rgb(0, 0, 0);")
+        self.btnEdit.setText("")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("Interface/Images/edit.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.btnEdit.setIcon(icon)
+        self.btnEdit.setIconSize(QtCore.QSize(40, 40))
+        self.btnEdit.setObjectName("btnEdit")
+        self.btnDelete = QtWidgets.QPushButton(Tabla)
+        self.btnDelete.setGeometry(QtCore.QRect(400, 300, 140, 60))
+        font = QtGui.QFont()
+        font.setFamily("Bitstream Vera Sans")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.btnDelete.setFont(font)
+        self.btnDelete.setStyleSheet("background-color: qlineargradient(spread:reflect, x1:0, y1:0.882, x2:0.851, y2:0, stop:0 rgba(204, 0, 0, 0), stop:0.273632 rgba(255, 255, 255, 255));\n"
+"background-color: rgb(255, 40, 40);\n"
+"color: rgb(0, 0, 0);\n"
+"border-style: solid;\n"
+"border-width:2px;\n"
+"border-radius:20px;\n"
+"border-color: rgb(0, 0, 0);")
+        self.btnDelete.setText("")
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap("Interface/Images/121113.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.btnDelete.setIcon(icon1)
+        self.btnDelete.setIconSize(QtCore.QSize(40, 40))
+        self.btnDelete.setAutoDefault(False)
+        self.btnDelete.setObjectName("btnDelete")
+        self.txtNumber = QtWidgets.QTextEdit(Tabla)
+        self.txtNumber.setGeometry(QtCore.QRect(100, 310, 111, 41))
+        font = QtGui.QFont()
+        font.setFamily("Bitstream Vera Sans")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.txtNumber.setFont(font)
+        self.txtNumber.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.txtNumber.setObjectName("txtNumber")
+        self.label = QtWidgets.QLabel(Tabla)
+        self.label.setGeometry(QtCore.QRect(60, 320, 41, 17))
+        font = QtGui.QFont()
+        font.setFamily("Bitstream Vera Sans")
+        font.setPointSize(14)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setStyleSheet("color: rgb(0, 0, 0);")
+        self.label.setObjectName("label")
+
+        self.retranslateUi(Tabla)
+        QtCore.QMetaObject.connectSlotsByName(Tabla)
+
+    def retranslateUi(self, Tabla):
+        _translate = QtCore.QCoreApplication.translate
+        Tabla.setWindowTitle(_translate("Tabla", "Editar Inventario"))
+        self.label.setText(_translate("Tabla", "No."))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Tabla = QtWidgets.QWidget()
+    ui = Ui_Tabla()
+    ui.setupUi(Tabla)
+    Tabla.show()
+    sys.exit(app.exec_())
+
---
+++ Unidad I/PAC III 2019/Proyecto 1/Python/main.py
@@ -0,0 +1,41 @@
+# -*- coding: utf8 -*-
+from PyQt5 import uic, QtWidgets
+from Interface.MainWindow import *
+from Interface.AddWindow import *
+from Interface.EditWindow import *
+
+class MainWindow(QtWidgets.QMainWindow,Ui_MainWindow):
+    #Pantalla Principal
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        self.setupUi(self)
+        self.ui=Ui_MainWindow()
+        self.center()
+        self.btnAdd.clicked.connect(self.openAdd)
+        self.btnEdit.clicked.connect(self.openEdit)
+
+    def openAdd(self):
+        self.AddWindow=QtWidgets.QWidget()
+        self.ui=Ui_WinAdd()      
+        self.ui.setupUi(self.AddWindow)
+        MainWindow.center(self.AddWindow)
+        self.AddWindow.show()
+
+    def openEdit(self):
+        self.EditWin=QtWidgets.QWidget()
+        self.ui=Ui_Tabla()      
+        self.ui.setupUi(self.EditWin)
+        MainWindow.center(self.EditWin)
+        self.EditWin.show()
+    
+    def center(self):
+        frame = self.frameGeometry()
+        centerPoint = QtWidgets.QDesktopWidget().availableGeometry().center()
+        frame.moveCenter(centerPoint)
+        self.move(frame.topLeft())
+
+if __name__ == "__main__":
+    apt = QtWidgets.QApplication([])
+    window = MainWindow()
+    window.show()
+    apt.exec()
