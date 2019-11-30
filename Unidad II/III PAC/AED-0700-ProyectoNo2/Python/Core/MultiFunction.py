c937df971ac0f222aafd794c2689e6fcf8722124
benedetto597 <ejbg597@gmail.com>
2019-11-30 12:06:39 -0600

    multifunction

--- Unidad II/III PAC/AED-0700-ProyectoNo2/Python/Core/MultiFunction.py
+++ Unidad II/III PAC/AED-0700-ProyectoNo2/Python/Core/MultiFunction.py
@@ -12,14 +12,14 @@ class Function:
             "%s%s" % ("\t", "-" * 100),
             "\n%s%s%s" % (
                 "\t%s" % (("*" * 11).center(100, " ")),
-                "\n\t%s" % ("* SGestor *".center(100, " ")),
+                "\n\t%s" % ("* Simulador de Gestor de Archivos  *".center(100, " ")),
                 "\n\t%s\n" % (("*" * 11).center(100, " "))
             ),
             "\n\t%s\n" % ("Sistema gestor de archivos".center(100, " ")),
             "\n\t%s" % ("Creado por: ".center(100, " ")),
             "\n\t%s" % ("[Bryan Gonzales] [Edgar Benedetto] [Fabio Lagos]".center(100, " ")),
             "\n\t%s\n" % ("v 0.00.10".center(100, " ")),
-            "\n\tSistema que gestiona archivos almacenados en formato JSON.\n",
+            "\n\tSistema que gestiona archivos y carpetas en consola exclusivamente, almacenados en formato JSON.\n",
             "%s%s" % ("\t", "-" * 100)
         )
         print(header)
@@ -38,26 +38,31 @@ class Function:
             #"%s%s" % ("\t", "-" * 100),
             "\tComandos:\n",
             "\n\t\thelp\t---> Ayuda.",
-            "\n\t\tls\t---> Muestra algo en la consola.",
-            "\n\t\texit\t---> Salir del programa.\n",
+            "\n\t\tls\t---> Imprime una lista de forma horizontal, con lo existente en la ruta actual.",
+            "\n\t\tls-1\t---> Imprime una lista de forma vertical, con lo existente en la ruta actual.\n",
+            "\n\t\tls-1\t---> Imprime una lista de forma vertical, con lo existente en la ruta actual.\n",
+            "\n\t\tpwd\t---> Impresión de la ruta actual en la que se encuentra.\n",
+            "\n\t\ttouch\t---> Crea un Archivo.\n",
+            "\n\t\tmkdir\t---> Crea un Directorio.\n",
+            "\n\t\tplot\t---> Crea un Directorio.\n",
+            "\n\t\trm\t---> Elimina un archivo.\n",
+            "\n\t\trmdir\t---> Elimina un carpeta.\n",
+            "\n\t\ttrash\t---> Lista de archivos y directorios borrados con su nombre y fecha.\n",
+            "\n\t\tcd\t---> Para navergar en rutas.\n",
+            "\n\t\tfindfbe\t---> Encontrar archivos por extención.\n",
             #"%s%s" % ("\t", "-" * 100),
         )
         print(help)
-
-    def clean(self, command):
-        return []
     
     def ls(self):
         pass
 
     def plot(self):
         app = QtWidgets.QApplication([])
-
         self.window = QtWidgets.QMainWindow()
         self.goWindow = Graph()
         self.goWindow.setupUi(self.window)
         self.window.show()
-
         app.exec()
     
     def mkdir(self, command):
@@ -73,7 +78,7 @@ class Function:
         array = []
 
         for i in text:
-            a = []
+            newArray = []
             i = i.strip()
 
             space = i.find(" ")
@@ -82,8 +87,8 @@ class Function:
             else: 
                 command, param = i[:space],i[space:]
 
-            a.append(command)
-            a.append(param.strip())
-            array.append(a)
+            newArray.append(command)
+            newArray.append(param.strip())
+            array.append(newArray)
 
         return array
